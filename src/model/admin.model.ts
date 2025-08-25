8658529dc60b4d220e72988268891d056ced65cb
Umesh Joshi <uj19343@gmail.com>
2025-08-25 21:54:06 +0545

    admin model Ready

---
+++ src/model/admin.model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const emailRegex = /^\S+@\S+\.\S+$/;
+
+const adminSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Firstname is required."],
+    minLength: [3, "Firstname must me more that 3 character."],
+    maxLength: [50, "Firstname must be less than 50 character."],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Lastname is required."],
+    minLength: [3, "Lastname must me more that 3 character."],
+    maxLength: [50, "Lastname must be less than 50 character."],
+  },
+  role: {
+    type: String,
+    required: [true, "Role must be required of user."],
+    default: "Admin",
+  },
+  email: {
+    type: String,
+    required: [true, "Email required for user."],
+    match: emailRegex,
+  },
+  password: {
+    type: String,
+    minLength: [6, "Password more than 6 character."],
+    maxLength: [50, "Password must be less than 50 character."],
+  },
+});
+
+const AdminRegister = mongoose.model("Admin", adminSchema);
+
+export default AdminRegister;
