43f2246fe3d4f3443c6f557c8cefc7ce97f38ec3
Umesh Joshi <uj19343@gmail.com>
2025-08-26 23:50:23 +0545

    setup th ejson web token

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -3,6 +3,8 @@ import Admin from "../model/admin.model";
 import CustomError from "../middleware/customErrorHandle.middleware";
 import { comparePassword, hashPassword } from "../utils/bcrypt.utils";
 import { uploadFile } from "../config/Cloudinary.config";
+import { generateWebToken } from "../utils/jwt.utils";
+import { IPayload, Role } from "../@types/global.types";
 
 export const adminRegister = async (req: Request, res: Response) => {
   try {
@@ -79,7 +81,7 @@ export const adminLogin = async (req: Request, res: Response) => {
 
   const admin = await Admin.findOne({ email });
 
-  if (!admin) {
+  if (!admin || admin.role !== Role.superAdmin) {
     throw new CustomError("user doesnt exists.", 404);
   }
 
@@ -93,6 +95,15 @@ export const adminLogin = async (req: Request, res: Response) => {
     throw new CustomError("User doesnt exists", 404);
   }
 
+  const payload: IPayload = {
+    _id: admin._id,
+    email: admin.email,
+    firstName: admin.firstName,
+    lastName: admin.lastName,
+    role: admin.role,
+  };
+
+  const token = await generateWebToken(payload);
   res.status(200).json({
     messgae: "User Login sucessfully.",
     status: "success",
---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { IPayload } from "../@types/global.types";
+
+const JWT_SECRET_TOKEN = process.env.JWT_SECRECT_TOKEN || "";
+const JWT_TOKEN_EXPIRES = process.env.JWT_TOKEN_EXPIRES || "1d";
+
+export const generateWebToken = (payload: IPayload) => {
+  if (!JWT_SECRET_TOKEN) {
+    throw new Error("JWT_SECRET_TOKEN is not defined");
+  }
+
+  return jwt.sign(payload, JWT_SECRET_TOKEN, {
+    expiresIn: JWT_TOKEN_EXPIRES,
+  });
+};
