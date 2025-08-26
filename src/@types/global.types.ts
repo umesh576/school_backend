11170a6d53d62bc44d174eef1ec8ccf3ec7cac92
Umesh Joshi <uj19343@gmail.com>
2025-08-26 19:10:55 +0545

    jason webtoken genetrating

---
+++ src/@types/global.types.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export enum Role {
+  superAdmin = "SUPERADMIN",
+  managment = "management",
+  staff = "STAFF",
+  student = "STUDENT",
+}
+
+export const onlySuperAdmin = Role.superAdmin;
+export const onlyManagment = Role.student;
+export const onlyStudent = Role.student;
+export const onlyStaff = Role.staff;
+
+export interface IPayload {
+  _id: mongoose.Types.ObjectId;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: Role;
+}
