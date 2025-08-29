1ec51a2152342c18272f8bc3efa9f94111bcffd1
Umesh Joshi <uj19343@gmail.com>
2025-08-29 21:08:30 +0545

    new gjr

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { adminLogin, adminRegister } from "../controller/admin.controller";
 import multer from "multer";
-// import { staffLogin } from "./../controller/admin.controller";
+import { staffLogin } from "./../controller/admin.controller";
 
 const upload = multer({ storage: multer.diskStorage({}) });
 const server = Router();
 
 server.post("/register", upload.single("profilePicture"), adminRegister);
 server.post("/admin/login", adminLogin);
-// server.post("/staff/login"), staffLogin;
+server.post("/staff/login"), staffLogin;
 export default server;
