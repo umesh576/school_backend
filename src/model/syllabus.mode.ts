792db51ea3a75550c1ae357e0040276d6bd7c798
Umesh Joshi <uj19343@gmail.com>
2025-09-15 00:58:26 +0545

    working on syllabus api

--- src/model/syllabus.mode.ts
+++ src/model/syllabus.mode.ts
@@ -1,1 +1,25 @@
 import mongoose from "mongoose";
+
+const syllabusSchema = new mongoose.Schema(
+  {
+    Heading: {
+      type: String,
+      required: "Heading is required.",
+      min: [5, "Heading must Have 5 Character"],
+    },
+    content: {
+      type: String,
+      required: "Heading is required.",
+      min: [5, "Content must Have 5 Character"],
+    },
+    subHeading: {
+      type: String,
+      min: [5, "Sub heading must Have 5 Character"],
+    },
+    subContent: {
+      type: String,
+      min: [5, "Sub content must Have 5 Character"],
+    },
+  },
+  { timestamps: true }
+);
